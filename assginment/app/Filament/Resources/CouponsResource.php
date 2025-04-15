83473127391a279c0be9b5285da8df22e961eef3
Duongvuhia30 <duongvuhai26@gmail.com>
2025-04-15 23:49:25 +0700

    [admin] update blog, categoryblogcategoryblog

---
+++ assginment/app/Filament/Resources/CouponsResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CouponsResource\Pages;
+use App\Filament\Resources\CouponsResource\RelationManagers;
+use App\Models\Coupons;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CouponsResource extends Resource
+{
+    protected static ?string $model = Coupons::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCoupons::route('/'),
+            'create' => Pages\CreateCoupons::route('/create'),
+            'edit' => Pages\EditCoupons::route('/{record}/edit'),
+        ];
+    }
+}
