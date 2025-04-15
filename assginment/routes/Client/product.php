557027360f245282b3023ec47ec593aca779735f
Duongvuhia30 <duongvuhai26@gmail.com>
2025-04-16 00:57:55 +0700

    Reapply "Merge branch 'duong' of https://github.com/Duongvuhia30/PC09408-PHP3-ASM into duong"
    This reverts commit 5a1cf78e2b6672340946644f890d1603b248dac1.

--- assginment/routes/Client/product.php
+++ assginment/routes/Client/product.php
@@ -5,5 +5,5 @@ use App\Livewire\Client\Products;
 use App\Livewire\Client\ProductDetail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [Products::class, 'products']);
+Route::get('products',[Products::class,"showProductsByCategory"]);
 
---
+++ assginment/routes/Client/productdetail.php
@@ -0,0 +1,8 @@
+<?php 
+
+
+use App\Livewire\Client\Products;
+use App\Livewire\Client\ProductDetail;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/productdetail/{id}',[Products::class,"Detail"]);
